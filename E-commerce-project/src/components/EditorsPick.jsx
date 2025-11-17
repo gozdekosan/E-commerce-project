2ad7fc4a4c1852abd2cac22736c223702860c956
gozdekosan <gkosann@gmail.com>
2025-11-17 12:23:35 +0300

    fix: define editorsProducts

--- E-commerce-project/src/components/EditorsPick.jsx
+++ E-commerce-project/src/components/EditorsPick.jsx
@@ -1,5 +1,5 @@
 
- editorsProducts = [
+ const editorsProducts = [
   { id: 1, title: "MEN", link: "/shop/men", image: "src/images/men.png", size: "large" }, 
   { id: 2, title: "WOMEN", link: "/shop/women", image: "src/images/women.png", size: "tall" }, 
   { id: 3, title: "ACCESSORIES", link: "/shop/accessories", image: "src/images/accessories.png", size: "half-tall" }, 
