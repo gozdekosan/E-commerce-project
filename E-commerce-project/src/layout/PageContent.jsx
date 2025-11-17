7b61c3dfa0cfc49d685f960171d9ceaa597b3077
gozdekosan <gkosann@gmail.com>
2025-11-17 18:37:46 +0300

    fix react-router-dom latest version

--- E-commerce-project/src/layout/PageContent.jsx
+++ E-commerce-project/src/layout/PageContent.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { 
+  BrowserRouter, 
+  Routes,      
+  Route        
+} from "react-router-dom";
+
 import HomePage from "../pages/HomePage.jsx";
 import LoginPage from "../pages/LoginPage.jsx";
 import RegisterPage from "../pages/RegisterPage.jsx";
@@ -6,15 +11,17 @@ import RegisterPage from "../pages/RegisterPage.jsx";
 export default function PageContent() {
   return (
     <div className="w-full min-h-screen bg-white">
-    <main className="flex-1 w-full max-w-none">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-        </Switch>
-      </Router>
-    </main>
+      <main className="flex-1 w-full max-w-none">
+    
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
